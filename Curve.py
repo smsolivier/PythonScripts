8deb0cd7a919d6076cd3d2641f8db6c984f4ea95
Sam Olivier <smsolivier@gmail.com>
2018-03-12 11:38:04 -0700

    added write frequency

--- Curve.py
+++ Curve.py
@@ -1,12 +1,17 @@
 class Curve:
 
-	def __init__(self, name):
+	def __init__(self, name, freq=1):
 
 		self.name = name 
 		self.counter = 0
+		self.freq = freq 
 
 	def write(self, x, u):
 		''' write to curve format for Visit ''' 
+
+		if (self.counter%self.freq != 0):
+			self.counter += 1
+			return 
 
 		f = open(self.name+str(self.counter)+'.curve', 'w')
 		f.write('# var\n') 
