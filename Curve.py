eb35e9a9d275d5d19a8cb9191604eaee87207570
Sam Olivier <smsolivier@gmail.com>
2018-09-06 09:20:47 -0700

    write multiple curves at once

--- Curve.py
+++ Curve.py
@@ -6,17 +6,20 @@ class Curve:
 		self.counter = 0
 		self.freq = freq 
 
-	def write(self, x, u):
+	def write(self, x, *args):
 		''' write to curve format for Visit ''' 
+
+		n = len(args) 
 
 		if (self.counter%self.freq != 0):
 			self.counter += 1
 			return 
 
 		f = open(self.name+str(self.counter)+'.curve', 'w')
-		f.write('# var\n') 
-		for i in range(len(x)):
-			f.write('{} {}\n'.format(x[i], u[i]))
+		for var in range(n):
+			f.write('# var' + str(var) + '\n') 
+			for i in range(len(x)):
+				f.write('{} {}\n'.format(x[i], args[var][i]))
 
 		f.close()
 
