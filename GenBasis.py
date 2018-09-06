755c659830499e57fd48ecf3f58d5412bcf35a9e
Sam Olivier <smsolivier@gmail.com>
2018-09-06 09:21:04 -0700

    solve for 1D basis functions and their derivatives

---
+++ GenBasis.py
@@ -0,0 +1,27 @@
+import numpy as np 
+
+def genBasis(p, a=0, b=1):
+	N = p + 1
+	x = np.linspace(a, b, N)
+	coef = np.zeros((N, N))
+
+	for k in range(N):
+		A = np.zeros((N, N))
+		for i in range(N):
+			for j in range(N):
+				A[i,j] = x[i]**j
+
+		b = np.zeros(N)
+		b[k] = 1 
+
+		coef[k,:] = np.linalg.solve(A, b)
+
+	B = [] 
+	dB = [] 
+
+	x2 = np.linspace(-1, 1)
+	for i in range(N):
+		B.append(np.poly1d(coef[i,::-1]))
+		dB.append(B[i].deriv())
+
+	return B, dB
