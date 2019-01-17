5ae8170673c4d7f06e308530c1d252afcd1e65d4
Sam Olivier <smsolivier@gmail.com>
2019-01-17 14:16:27 -0800

    fixed formatting

--- texTools/utils.py
+++ texTools/utils.py
@@ -1,6 +1,6 @@
 ''' general utilities for texTools ''' 
 
-def writeNumber(x, fmt='{:.3}'):
+def writeNumber(x, fmt='{:.3f}'):
 	''' Returns the string in Latex format: 
 			\num{fmt.format(x)} 
 	''' 
