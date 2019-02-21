25b5f00d583e73cfad9f0947bf7901fbeea0e3a8
Sam Olivier <smsolivier@gmail.com>
2019-02-20 16:02:46 -0800

    name files sequentially

--- Curve.py
+++ Curve.py
@@ -5,6 +5,7 @@ class Curve:
 		self.name = name 
 		self.counter = 0
 		self.freq = freq 
+		self.writes = 0
 
 	def write(self, x, *args):
 		''' write to curve format for Visit ''' 
@@ -15,12 +16,16 @@ class Curve:
 			self.counter += 1
 			return 
 
-		f = open(self.name+str(self.counter)+'.curve', 'w')
+		f = open(self.name+str(self.writes)+'.curve', 'w')
 		for var in range(n):
 			f.write('# var' + str(var) + '\n') 
 			for i in range(len(x)):
-				f.write('{} {}\n'.format(x[i], args[var][i]))
+				val = 0 
+				if (abs(args[var][i]) > 1e-18):
+					val = args[var][i]
+				f.write('{} {}\n'.format(x[i], val))
 
 		f.close()
 
 		self.counter += 1 
+		self.writes += 1
