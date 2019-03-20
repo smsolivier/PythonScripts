44eb9bb7f86ce235c2317fd6740ca946df7e801c
Sam Olivier <smsolivier@gmail.com>
2019-03-20 11:53:58 -0700

    added timing per each loop

--- ProgressBar.py
+++ ProgressBar.py
@@ -6,16 +6,18 @@ import Timer
 class progressbar:
 	j = 0 # store iteration number 
 
-	def __init__(self, max, time=False, width=50):
+	def __init__(self, max, TIME=False, TIME2=False, width=50):
 		self.max = max # max number of iterations 
 		self.width = width # width of progress bar 
-		self.time = time
+		self.time = TIME
+		self.time2 = TIME2 
 
 		if (self.time):
 			self.tt = Timer.timer(' ')
 
 		# start bar 
 		print('[>' + ' '*(self.width-1) + '] 0%\r', end='')
+		self.prev = time.time() 
 
 	def update(self):
 		self.j += 1 # update iteration counter 
@@ -36,7 +38,13 @@ class progressbar:
 		output('] ' + '{:.0f}'.format(percent*100) + '%')
 
 		if (self.j < self.max):
-			print('\r', end='', flush=True) # continue line 
+			if (self.time2):
+				print(' {:.3f} s/iter\r'.format(
+					time.time() - self.prev), end='', flush=True)
+				self.prev = time.time() 
+			else:
+				print('\r', end='', flush=True) # continue line 
+
 		elif (self.j == self.max):
 			if (self.time):
 				self.tt.stop()
@@ -44,3 +52,10 @@ class progressbar:
 				print()
 		else:
 			print('too many')
+
+if __name__=='__main__':
+	N = 10
+	bar = progressbar(N, True, True) 
+	for i in range(N):
+		time.sleep(.75) 
+		bar.update() 
